ef6e531a15144f844b8383e5283ede496df87b16
ghazwanz <ababilghazwan@gmail.com>
2026-02-24 20:44:40 +0700

    Jobsheet 2 Praktikum 3

--- jobsheet-2/routes/web.php
+++ jobsheet-2/routes/web.php
@@ -65,3 +65,13 @@ Route::resource('photos', PhotoController::class)->except([
     'create', 'store', 'update', 'destroy'
 ]);
 
+// Route::get('/greeting', function () {
+//     return view('hello', ['name' => 'Ghazwan']);
+// });
+
+// Route::get('/greeting', function () {
+//     return view('blog.hello', ['name' => 'Ghazwan']);
+// });
+
+Route::get('/greeting', [WelcomeController::class, 'greeting']);
+
