0d6b5dac490ca1c1d70ea6538679f237e011a732
ghazwanz <ababilghazwan@gmail.com>
2026-02-24 20:09:52 +0700

    Jobsheet 2 Praktikum 2

--- jobsheet-2/routes/web.php
+++ jobsheet-2/routes/web.php
@@ -1,14 +1,20 @@
 <?php
 
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\PageController;
+use App\Http\Controllers\PhotoController;
+use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return 'Selamat Datang';
-});
+// Route::get('/', function () {
+//     return 'Selamat Datang';
+// });
 
-Route::get('/hello', function () {
-    return "Hello World";
-});
+// Route::get('/hello', function () {
+//     return "Hello World";
+// });
 
 Route::get('/world', function () {
     return "World";
@@ -27,9 +33,9 @@ Route::get('/posts/{post}/comments/{comment}', function ($postId,$commentsId) {
     return "Pos ke-$postId, Komentar ke-$commentsId";
 });
 
-Route::get('/articles/{id}', function ($id) {
-    return "Halaman Artikel dengan ID $id";
-});
+// Route::get('/articles/{id}', function ($id) {
+//     return "Halaman Artikel dengan ID $id";
+// });
 
 // Route::get('users/{name?}', function ($name=null) {
 //     return "Nama Saya $name";
@@ -38,3 +44,24 @@ Route::get('/articles/{id}', function ($id) {
 Route::get('users/{name?}', function ($name="John") {
     return "Nama Saya $name";
 });
+
+Route::get('/hello',[WelcomeController::class,'hello']);
+
+// Route::get('/', [PageController::class,'index']);
+// Route::get('/about', [PageController::class,'about']);
+// Route::get('/articles/{id}', [PageController::class,'articles']);
+
+Route::get('/', [HomeController::class,'index']);
+Route::get('/about', [AboutController::class,'index']);
+Route::get('/articles/{id}', [ArticleController::class,'index']);
+
+// Route::resource('photos', PhotoController::class);
+
+Route::resource('photos', PhotoController::class)->only([
+    'index', 'show'
+]);
+
+Route::resource('photos', PhotoController::class)->except([
+    'create', 'store', 'update', 'destroy'
+]);
+
