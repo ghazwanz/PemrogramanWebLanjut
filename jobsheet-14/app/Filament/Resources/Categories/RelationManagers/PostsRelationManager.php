014f81e19f4fa91ee7390b25771f5c5542e34912
ghazwanz <ababilghazwan@gmail.com>
2026-06-01 22:10:20 +0700

    Jobsheet 14

---
+++ jobsheet-14/app/Filament/Resources/Categories/RelationManagers/PostsRelationManager.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Filament\Resources\Categories\RelationManagers;
+
+use App\Filament\Resources\Posts\Schemas\PostForm;
+use Filament\Forms\Components\TextInput;
+use Filament\Schemas\Schema;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Actions\CreateAction;
+use Filament\Actions\EditAction;
+use Filament\Actions\DeleteAction;
+use Filament\Actions\BulkActionGroup;
+use Filament\Actions\DeleteBulkAction;
+use Filament\Resources\RelationManagers\RelationManager;
+use Filament\Tables\Table;
+
+class PostsRelationManager extends RelationManager
+{
+    protected static string $relationship = 'posts';
+
+    public function form(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                TextInput::make('title')
+                    ->required()
+                    ->maxLength(255),
+                TextInput::make('slug')
+                    ->required()
+                    ->maxLength(255),
+            ]);
+    }
+
+    public function table(Table $table): Table
+    {
+        return $table
+            ->recordTitleAttribute('title')
+            ->columns([
+                TextColumn::make('title'),
+                TextColumn::make('slug'),
+                TextColumn::make('created_at')->dateTime(),
+            ])
+            ->headerActions([
+                CreateAction::make(),
+            ])
+            ->recordActions([
+                EditAction::make(),
+                DeleteAction::make(),
+            ])
+            ->bulkActions([
+                BulkActionGroup::make([
+                    DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+}
